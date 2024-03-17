034eb369e3cce6d0b58c25a2ec29d02c6c007a55
Jtility <leejewon97@gmail.com>
2024-03-17 22:56:51 +0900

    Command.cpp 에 함수 뼈대 구현 및 cmdJOIN 주석(계획) 추가

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,4 +1,30 @@
 
 #include "Command.hpp"
 
+void Command::cmdJOIN(Client &client, std::string params) {
+    (void)client;
+    // param : JOIN #channel1,#channel2 key1,key2   (key is optional)
+    // 1. check if channel exists
+    // 2. check if user is already in channel
+    // 3. check if channel is full
+    // 4. check if channel is invite only
+    // 5. check if channel is banned
+    // 6. check if channel is moderated
+    // 7. check if channel is +i or +t or +k or +o or +l
+    // 8. join channel
+}
 
+void Command::cmdWHO(Client &client, std::string params) {
+    (void)client;
+    (void)params;
+}
+
+void Command::cmdMODE(Client &client, std::string params) {
+    (void)client;
+    (void)params;
+}
+
+void Command::cmdINVITE(Client &client, std::string params) {
+    (void)client;
+    (void)params;
+}
