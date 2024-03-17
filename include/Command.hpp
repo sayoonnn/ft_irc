eb70001f18296139d759de321fd36fb677c53ee5
Jtility <leejewon97@gmail.com>
2024-03-17 22:41:30 +0900

    jaewolee 담당 Command 멤버변수 함수 원형 정의 branch jaewolee 생성

--- include/Command.hpp
+++ include/Command.hpp
@@ -1,10 +1,15 @@
 
 #pragma once
 
+#include "Client.hpp"
+
 class Command {
 
 	private:
-
+		void cmdJOIN(Client &, std::string);
+		void cmdWHO(Client &, std::string);
+		void cmdMODE(Client &, std::string);
+		void cmdINVITE(Client &, std::string);
 	public:
 
 		
