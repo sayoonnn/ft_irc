baae4d0b3ca7702d0aa6b42f680156170d0a17b4
Sangjeong Yoon <sayoon@cx2r4s2.42seoul.kr>
2024-03-19 22:10:01 +0900

    24/03/19 22:10

--- include/RPL.hpp
+++ include/RPL.hpp
@@ -2,16 +2,16 @@
 #pragma once
 
 // Welcome
-#define RPL_WELCOME(nickname)					(":ircserv 001 " + (nickname) + " :Welcome to the ircserv, " + (nickname) + "\n")
-#define RPL_YOURHOST(nickname)					(":ircserv 002 " + (nickname) + " :Your host is ircserv, running version 1.0\n")
-#define RPL_CREATED(nickname, datetime)			(":ircserv 003 " + (nickname) + " :This server was created " + (datetime)  + "\n")
-#define RPL_MYINFO(nickname)					(":ircserv 004 " + (nickname) + " ircserv 1.0 aw abeiIklmnoOt\n")
+#define RPL_WELCOME(nickname)					(":ircserv 001 " + (nickname) + " :Welcome to the ircserv, " + (nickname) + "\r\n")
+#define RPL_YOURHOST(nickname)					(":ircserv 002 " + (nickname) + " :Your host is ircserv, running version 1.0\r\n")
+#define RPL_CREATED(nickname, datetime)			(":ircserv 003 " + (nickname) + " :This server was created " + (datetime)  + "\r\n")
+#define RPL_MYINFO(nickname)					(":ircserv 004 " + (nickname) + " ircserv 1.0 aw abeiIklmnoOt\r\n")
 
 
 // errors
-#define ERR_UNKNOWNCOMMAND(nickname, command)	(":ircserv 421 " + (nickname) + " " + (command) + " :Unknown command")
-#define ERR_NICKNAMEINUSE(realname, nickname)	(":ircserv 433 " + (realname) + " " + (nickname) + " :Nickname is already in use")
-#define ERR_NOTREGISTERED(realname)				(":ircserv 451 " + (realname) + " :You have not registered")
-#define ERR_NEEDMOREPARAMS(nickname, command)	(":ircserv 461 " + (nickname) + " " + (command) + " :Not enough parameters")
-#define ERR_ALREADYREGISTERED(nickname)			(":ircserv 462 " + (nickname) + " :You may not reregister")
-#define ERR_PASSWDMISMATCH(nickname)			(":ircserv 464 " + (nickname) + " :Password incorrect")
+#define ERR_UNKNOWNCOMMAND(nickname, command)	(":ircserv 421 " + (nickname) + " " + (command) + " :Unknown command\r\n")
+#define ERR_NICKNAMEINUSE(realname, nickname)	(":ircserv 433 " + (realname) + " " + (nickname) + " :Nickname is already in use\r\n")
+#define ERR_NOTREGISTERED(realname)				(":ircserv 451 " + (realname) + " :You have not registered\r\n")
+#define ERR_NEEDMOREPARAMS(nickname, command)	(":ircserv 461 " + (nickname) + " " + (command) + " :Not enough parameters\r\n")
+#define ERR_ALREADYREGISTERED(nickname)			(":ircserv 462 " + (nickname) + " :You may not reregister\r\n")
+#define ERR_PASSWDMISMATCH(nickname)			(":ircserv 464 " + (nickname) + " :Password incorrect\r\n")
