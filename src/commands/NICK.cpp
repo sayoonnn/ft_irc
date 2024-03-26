e23ce2660e8431ea79f1eeaf7ea7f196726330fe
Sangjeong Yoon <sayoon@cx2r5s5.42seoul.kr>
2024-03-26 15:32:01 +0900

    24/03/26 15:32 nick 수정

--- src/commands/NICK.cpp
+++ src/commands/NICK.cpp
@@ -9,10 +9,10 @@ void Server::NICK(std::deque<std::string> &parsedCmd, Client &client) {
 		return ;
 	}
 
-	std::map<int, Client *>::iterator it = _clients.begin();
+	std::map<int, Client *>::iterator socketIt = _clients.begin();
 
-	for (; it != _clients.end(); it++) {
-		if ((*it).second->getNickname() == parsedCmd[1] && (*it).second != &client) {
+	for (; socketIt != _clients.end(); socketIt++) {
+		if ((*socketIt).second->getNickname() == parsedCmd[1] && (*socketIt).second != &client) {
 			sendMessageToClient(client.getSocket(), ERR_NICKNAMEINUSE(client.getUsername(), parsedCmd[1]));
 			return ;
 		}
@@ -22,6 +22,17 @@ void Server::NICK(std::deque<std::string> &parsedCmd, Client &client) {
 	parsedCmd[1].find("#") != std::string::npos || parsedCmd[1].find(",") != std::string::npos) {
 		sendMessageToClient(client.getSocket(), ERR_ERRONEUSNICKNAME(client.getNickname(), parsedCmd[1]));
 		return ;
+	}
+
+	std::map<std::string, Client *>::iterator nickIt = _clientsNick.find(client.getNickname());
+
+	if (nickIt == _clientsNick.end())
+		_clientsNick[parsedCmd[1]] = &client;
+	else {
+		_clientsNick.erase(nickIt);
+		_clientsNick[parsedCmd[1]] = &client;
+		std::string tmp = ":" + client.getNickname() + " NICK " + parsedCmd[1] + "\r\n"; 
+		sendMessageToClient(client.getSocket(), tmp);
 	}
 
 	client.setNickname(parsedCmd[1]);
--- src/commands/QUIT.cpp
+++ src/commands/QUIT.cpp
@@ -2,5 +2,9 @@
 #include "Server.hpp"
 #include "RPL.hpp"
 
-void Server::QUIT(std::deque<std::string> &parsedCmd, Client &client) { (void)parsedCmd, (void)client; }
+void Server::QUIT(std::deque<std::string> &parsedCmd, Client &client) {
+
+	(void)parsedCmd, (void)client;
+
+}
 
