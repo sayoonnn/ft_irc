c641949c02c8eb980ba593d124ae6c197a507742
Sangjeong Yoon <sayoon@cx2r4s2.42seoul.kr>
2024-03-28 15:26:58 +0900

    24/03/28 15:26 Fix: privmsg 수정

--- src/commands/PRIVMSG.cpp
+++ src/commands/PRIVMSG.cpp
@@ -26,6 +26,8 @@ void	Server::PRIVMSG(std::deque<std::string> &parsedCmd, Client &client)
 	if (target.find(",") != std::string::npos)
 		target = target.substr(0, target.find(","));
 
+	type = target[0];
+
 	send = RPL_PRIVMSG(client.getNickname(), client.getUsername(), target, parsedCmd[2]);
 	if (type == '#')
 	{
